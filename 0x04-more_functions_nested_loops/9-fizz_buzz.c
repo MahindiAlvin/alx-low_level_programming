90fe11b351ad6f33791bec9db0f176d6edcad2d6
MahindiAlvin <you@example.com>
2023-02-23 14:18:56 -0800

    Task 9 final answer

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,8 +22,7 @@ int main(void)
 	{
 	printf("Fizz");
 	}
-	else if
-	(x % 5 == 0);
+	else if (x % 5 == 0)
 	{
 	printf("Buzz");
 	}
@@ -31,8 +30,7 @@ int main(void)
 	{
 	printf("%d", x);
 	}
-	if
-	(x != 100);
+	if (x != 100)
 	{
 	printf(" ");
 	}
